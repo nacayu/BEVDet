c0ee751f8ad65f68452eb5ba84976b2ac6d1f8c8
Muwei <47105158+nacayu@users.noreply.github.com>
2023-05-14 18:51:46 +0800

    complete bevdet submission

--- configs/bevdet_occ/bevdet-occ-r50-4d-stereo-24e.py
+++ configs/bevdet_occ/bevdet-occ-r50-4d-stereo-24e.py
@@ -205,7 +205,7 @@ share_data_config = dict(
 
 test_data_config = dict(
     pipeline=test_pipeline,
-    ann_file=data_root + 'bevdetv2-nuscenes_infos_val.pkl')
+    ann_file=data_root + 'bevdetv2-nuscenes_infos_test.pkl')
 
 data = dict(
     samples_per_gpu=4,
